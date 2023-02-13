6087a69af4b8ba4e5ed88dc3bf700ba08448df2f
Jonathan Enrique <jonathan.ariel.enrique@gmail.com>
2023-02-12 21:50:43 -0300

    Subiendo proyecto a repo

---
+++ data/stock.js
@@ -0,0 +1,56 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "El club de las 5 de la mañana",
+        tipo: "libro",
+        precio: 3600,
+        imagen: '../public/images/elClubDeLas5.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Habitos Atomicos",
+        tipo: "libro",
+        precio: 2700,
+        imagen: '../public/images/habitosAtomicos.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Padre Rico Padre Pobre",
+        tipo: "libro",
+        precio: 1000,
+        imagen: '../public/images/padreRicoPadrePobre.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Despertando al gigante",
+        tipo: "libro",
+        precio: 2200,
+        imagen: '../public/images/despertandoAlGiganteInterior.png',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Mi portada de libro",
+        tipo: "libro",
+        precio: 1200,
+        imagen: '../public/images/miPortadaDeLibro.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Harry Potter",
+        tipo: "libro",
+        precio: 2700,
+        imagen: '../public/images/harryPotter.jpg',
+        cantidad:1
+    }
+];
